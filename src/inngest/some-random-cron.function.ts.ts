ba204e7286b7e3a6e59d76c5058b910d478928b2
Francis Berger <fberger@coinshares.com>
2024-10-22 18:29:17 +0200

    test: cron

--- src/inngest/some-random-cron.function.ts.ts
+++ src/inngest/some-random-cron.function.ts.ts
@@ -1,8 +1,10 @@
+import numeral from 'numeral'
 import { inngest } from './client'
 
+const format = 'D MMMM YYYY hh:mm:ss A'
 export const someRandomCron = inngest.createFunction({ id: 'some-random-cron' }, { cron: 'TZ=Europe/Paris * * * * *' }, async ({ event, step }) => {
-    await step.run('1', async () => console.log(Date.now(), 'step 1'))
-    await step.run('2', async () => console.log(Date.now(), 'step 2'))
-    await step.run('3', async () => console.log(Date.now(), 'step 3'))
-    return { event, body: `Done at ${Date.now()}` }
+    await step.run('1', async () => console.log(numeral().format(format), 'step 1'))
+    await step.run('2', async () => console.log(numeral().format(format), 'step 2'))
+    await step.run('3', async () => console.log(numeral().format(format), 'step 3'))
+    return { event, body: `Done at ${numeral().format(format)}` }
 })
