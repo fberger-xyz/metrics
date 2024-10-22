8ada7d3df6bfde817059b6739ebf0b9a8be80cc6
Francis Berger <fberger@coinshares.com>
2024-10-22 18:14:53 +0200

    test: cron

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,11 +1,11 @@
 import { inngest } from '@/inngest/client'
-import { prepareWeeklyDigest } from '@/inngest/some-random-cron.function.ts'
 import { helloWorld } from '@/inngest/hello-word.function'
+import { someRandomCron } from '@/inngest/some-random-cron.function.ts'
 import { serve } from 'inngest/next'
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [helloWorld, prepareWeeklyDigest],
+    functions: [helloWorld, someRandomCron],
     streaming: 'allow',
 })
