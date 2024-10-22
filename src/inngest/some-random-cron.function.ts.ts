135c6e5a3d37eddd0337a74c5197ea0593a2348c
Francis Berger <fberger@coinshares.com>
2024-10-22 18:42:47 +0200

    test: cron

--- src/inngest/some-random-cron.function.ts.ts
+++ src/inngest/some-random-cron.function.ts.ts
@@ -1,6 +1,9 @@
 import dayjs from 'dayjs'
 import { inngest } from './client'
 import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
 dayjs.extend(timezone)
 
 const format = 'D MMMM YYYY hh:mm:ss A'
