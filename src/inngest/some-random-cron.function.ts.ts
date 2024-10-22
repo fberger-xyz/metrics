8359fcd17d9b266e855ff8f35beeb247ca5211fc
Francis Berger <fberger@coinshares.com>
2024-10-22 18:13:10 +0200

    test: cron

---
+++ src/inngest/some-random-cron.function.ts.ts
@@ -0,0 +1,8 @@
+import { inngest } from './client'
+
+export const someRandomCron = inngest.createFunction({ id: 'some-random-cron' }, { event: 'test/some-random-cron' }, async ({ event, step }) => {
+    await step.run('1', async () => console.log(Date.now(), 'step 1'))
+    await step.run('2', async () => console.log(Date.now(), 'step 2'))
+    await step.run('3', async () => console.log(Date.now(), 'step 3'))
+    return { event, body: `Done at ${Date.now()}` }
+})
