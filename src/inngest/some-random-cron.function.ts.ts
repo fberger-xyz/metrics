14e512f7a77f5d27fb7fec2f0a431481c8c9c73c
Francis Berger <fberger@coinshares.com>
2024-10-22 18:50:47 +0200

    test: cron

--- src/inngest/some-random-cron.function.ts.ts
+++ src/inngest/some-random-cron.function.ts.ts
@@ -8,7 +8,9 @@ dayjs.extend(timezone)
 
 const format = 'D MMMM YYYY hh:mm:ss A'
 const timestamp = () => dayjs().tz('Europe/Paris').format(format)
-export const someRandomCron = inngest.createFunction({ id: 'some-random-cron' }, { cron: 'TZ=Europe/Paris * * * * *' }, async ({ event, step }) => {
+
+// https://crontab.guru/every-5-minutes
+export const someRandomCron = inngest.createFunction({ id: 'some-random-cron' }, { cron: 'TZ=Europe/Paris */5 * * * *' }, async ({ event, step }) => {
     await step.run('1', async () => console.log(timestamp(), 'step 1'))
     await step.run('2', async () => console.log(timestamp(), 'step 2'))
     await step.run('3', async () => console.log(timestamp(), 'step 3'))
