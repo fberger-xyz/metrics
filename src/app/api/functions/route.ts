6361e2657239fc8928d3d293766f300120c1c97f
Francis Berger <fberger@coinshares.com>
2024-10-25 14:19:06 +0200

    fix: functions route

--- src/app/api/functions/route.ts
+++ src/app/api/functions/route.ts
@@ -1,9 +1,9 @@
 // https://vercel.com/docs/functions
 
-import type { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 
 export function GET(request: NextRequest) {
     const authHeader = request.headers.get('authorization')
     if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) return new Response('Unauthorized', { status: 401 })
-    return Response.json({ success: true })
+    return NextResponse.json({ success: true })
 }
